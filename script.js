561658111c5a9f634fc1ff554f71de81fbb71461
Kryzteena <92446773+Kryzteena@users.noreply.github.com>
2025-07-07 18:30:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const map = L.map('map').setView([13.41, 122.56], 6);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
+    attribution: '© OpenStreetMap contributors'
   }).addTo(map);
+
+  // Just to confirm it's working, add a marker
+  L.marker([13.41, 122.56]).addTo(map).bindPopup("Map Loaded").openPopup();
+});
 
 
   const markerClusterGroup = L.markerClusterGroup({
